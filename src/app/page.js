0dbbe7b2e69269e5af06acf10ed2aa3f3ec4d2b7
name <mahimathacker75@gmail.com>
2024-05-10 14:55:37 +0530

    done

---
+++ src/app/page.js
@@ -0,0 +1,11 @@
+"use client"; 
+import MemoryGame from "./components/MemoryGame";
+// import bot from './bot';
+
+export default function Home() {
+  return (
+   <div>
+    <MemoryGame />
+   </div>
+  );
+}
